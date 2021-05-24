dfb53a5e480fad381ed759f9f1db7f0b914e4490
Mitesh Ashar <email@miteshashar.com>
2021-05-24 07:30:16 +0530

    Improvements

--- getauth.py
+++ getauth.py
@@ -5,7 +5,8 @@ import sys
 import csv
 import json
 import os
-from datetime import date
+import urllib
+from datetime import date, datetime
 from datetime import timedelta
 from notifiers import get_notifier
 import configparser
@@ -26,14 +27,16 @@ weeks = int(config['DEFAULT']['weeks'])
 # print(telegramtoken)
 # print(cowinauth)
 
-notified_stale_token = False
+auth_errors = 0
 
 headers = {"authority":"cdn-api.co-vin.in" , "accept":"application/json text/plain, */*"   , "authorization":cowinauth   , "sec-ch-ua-mobile":"?0"   , "user-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"   , "origin":"https://selfregistration.cowin.gov.in"   , "sec-fetch-site":"cross-site" , "sec-fetch-mode":"cors" , "sec-fetch-dest":"empty", "referer":"https://selfregistration.cowin.gov.in/" ,"accept-language":"en-US,en-DE;q=0.9,en;q=0.8,de-DE;q=0.7,de;q=0.6,bn-IN;q=0.5,bn;q=0.4,hi-IN;q=0.3,hi;q=0.2"}
 
 mdict = {}
 while True:
     for distid in districtids.split(','):
-        print('District', distid)
+        print('================')
+        print('| DISTRICT', distid, '|')
+        print('================')
         try:
             today = date.today()
             for i in range(weeks):
@@ -46,12 +49,11 @@ while True:
                 try:
                     resj = r.json()
                 except json.decoder.JSONDecodeError:
-                    print('ERROR Decoding JSON')
-                    print(r.text)
-                    if r.text == 'Unauthenticated access!' and not notified_stale_token:
+                    print('Error decoding JSON: ' + r.text)
+                    auth_errors += 1
+                    if r.text == 'Unauthenticated access!' and auth_errors == 3:
                         print('Notifying stale token to admin')
                         telegram.notify(message='Please refresh token', token=telegramtoken, chat_id=personalchatid)
-                        notified_stale_token = True
                 count = 0
                 for center in resj['centers']:
                     for session in center['sessions']:
@@ -73,18 +75,19 @@ while True:
                         elif (cd['available'] > 0 and mdict[key] != cd):
                             count += 1
                             mdict[key] = cd
-                            message = [str(cd['pincode']) + ' ' + str(cd['min_age_limit']) + '+ ' + cd['vaccine'] + ' ' + cd['date']]
-                            message.append(str(cd['available']) + ' slots')
-                            message.append('Dose 1: ' + str(cd['available1']) + ', ' + 'Dose 2: ' + str(cd['available2']))
+                            vacci = {'COVAXIN': 'COVAX', 'COVISHIELD': 'COVI'}[cd['vaccine']]
+                            dt = datetime.strptime(cd['date'], '%d-%m-%Y').strftime('%d/%m')
+                            message = [str(cd['pincode']) + ' ' + vacci + ' ' + str(cd['min_age_limit']) + '+']
+                            message.append(dt + ': ' + str(cd['available']) + '(' + str(cd['available1']) + '+' + str(cd['available2']) + ') slots')
                             message.append(cd['name'] + ', ' + cd['district'])
-                            message.append(cd['address'])
                             telegram.notify(message='\n'.join(message), token=telegramtoken, chat_id=telegramchatid)
                             time.sleep(0.25)
                 print('Slots found: ' + str(count))
+                print('========================')
                 time.sleep(2)
         except Exception as e:
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             print(exc_type, fname, exc_tb.tb_lineno)
             time.sleep(5)
-    time.sleep(30)
+    time.sleep(15)
